842afdce9b21d3f7e66751b7c3d7fc2af03ef468
Evandro Assis <evandro.assis@nautilus.team>
2021-10-23 15:09:41 -0400

    feat: swagger documentation

--- src/modules/cars/Category/services/ImportCategoryService.ts
+++ src/modules/cars/Category/services/ImportCategoryService.ts
@@ -29,6 +29,7 @@ class ImportCategoryService {
           });
         })
         .on('end', () => {
+          fs.promises.unlink(file.path);
           resolve(categories);
         })
         .on('error', err => {
